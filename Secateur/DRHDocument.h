03b929452e5c86799729971705270ec41e94a310
Tanglo <developer@deadrubberhand.com>
2014-07-14 13:44:33 +1000

    Initial Commit

---
+++ Secateur/DRHDocument.h
@@ -0,0 +1,13 @@
+//
+//  DRHDocument.h
+//  Secateur
+//
+//  Created by Lee Walsh on 14/07/2014.
+//  Copyright (c) 2014 Dead Rubber Hand. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DRHDocument : NSDocument
+
+@end
---
+++ Secateur/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Secateur
+//
+//  Created by Lee Walsh on 14/07/2014.
+//  Copyright (c) 2014 Dead Rubber Hand. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
